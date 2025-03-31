205d33d361a76bbf579be5fa361c1a8d1352daf9
23f2002205 <23f2002205@ds.study.iitm.ac.in>
2025-03-31 20:05:17 +0530

    Add files via upload

---
+++ process_yt.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import re
+import httpx
+import os
+
+
+def get_transcript(question):
+    file_path = os.path.join(os.path.dirname(__file__), "transcript.xlsx")
+    df = pd.read_excel(file_path)
+    match = re.search(
+        r'between (\d+(?:\.\d+)?) and (\d+(?:\.\d+)?) seconds?', question, re.IGNORECASE)
+    if not match:
+        raise ValueError("Could not extract time range from the question.")
+    try:
+        start_time = int(float(match.group(1)))
+        end_time = int(float(match.group(2)))
+    except (ValueError, TypeError):
+        raise ValueError("Invalid time format extracted from the question.")
+
+    transcript = ""
+    for index, row in df.iterrows():
+        if row['timestamp'] >= start_time and row['timestamp'] <= end_time:
+            transcript += str(row['text'])+" "
+    # print(transcript)
+    return transcript
+
+
+API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjIwMDIyMDVAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.DJXQQZPQba2Ml2S4F3-YZEHbKdA6n08MY1_hKZ3vKRU"
+
+
+def correct_transcript(transcript):
+    BASE_URL = "https://aiproxy.sanand.workers.dev/openai/v1"
+    data = {
+        "model": "gpt-4o-mini",
+        "messages": [{"role": "user", "content": "correct this transcript: "+transcript+" return only the answer and nothing else , not extra text"}]
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
+    }
+    response = httpx.post(BASE_URL + "/chat/completions",
+                          json=data, headers=headers, timeout=60)
+
+    return response.json().get("choices", [])[0].get("message", {}).get("content")
+
+# question = "What is the text of the transcript of this Mystery Story Audiobook between 404.9 and 478.3 seconds?"
+# print(correct_transcript(get_transcript(question)))
